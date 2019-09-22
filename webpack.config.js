7a30db5a16e02d91b74ec9a0eca978cf73ea60c7
wuyueshengyan <1094562339@qq.com>
2019-09-22 11:21:11 +0800

    webpack-dev-server基本配置

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,14 @@ module.exports = {
     // development 开发阶段，打包
     mode: 'development',
     // 开启监视模式，此时执行webpack指令进行打包回监视文件的变化自动打包
-    watch: true
+    // watch: true
+    devServer: {
+        open: true,
+        port: 3000,
+        compress: true,
+        hot: true,
+        contentBase: './src'
+    }
 }
 
 // npx webpack -- cpnfig 指定你要用的自定义的配置文件
@@ -29,3 +36,17 @@ module.exports = {
 // "build":"webpack --config webpack.custom.config.js" webpack.custom.config.js 就是你要执行的自定义的配置文件
 // 这个时候你只需执行npm run build 就可以自动找到你packge.json上面的自定义指令进行打包
 //   }
+// 也可以使用webpack-dev-server 
+// 先要安装这个依赖，先要安装好webpack 他也是一个开发依赖，只有在开发中，我们才会遇到，
+// 安装 npm i webpack-dev-server -D
+// dev-server会在内存中生成一个js文件，不会在硬板中生成，第一次生成的时候，会提示你文件存放路径
+// dev-server 的作用非常简单，就是生成了一个express服务器，把我们的项目托管在上面。
+
+// webpack-dev-server --compress --hot  --port 5000 --open -- contentBase src
+// --hot 热模块更替  好处：每次更新之后，会以打补丁的方式更新，修改哪里，就更新哪里
+// --port 5000 5000端口运行
+// --open自动打开页面
+// --contentBase更改项目根目录
+// --compress 开启代码压缩
+
+// 还可以直接在webpack.config.js里面加这些方法，新增节点
